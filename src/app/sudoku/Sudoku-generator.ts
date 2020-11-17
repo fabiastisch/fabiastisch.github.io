280c7a43792e561afd5958fcfcb7a18e25a20d60
Fabian Weber <fabiandotweber@gmail.com>
2020-11-17 19:25:35 +0100

    WIP - try to generate a valid sudoku

--- src/app/sudoku/Sudoku-generator.ts
+++ src/app/sudoku/Sudoku-generator.ts
@@ -17,7 +17,38 @@ export class SudokuGenerator {
     return sudoku;
   }
 
-  private static canset(): boolean {
+  private static canset(num: number, column: number, numbers: number[], array: Cell[][]): boolean {
+    /*if (Math.floor(Math.random() + 0.5) === 1) {
+
+      return false;
+    }*/
+
+    // Check if Column contains the number
+    for (let row = 0; row < array.length; row++) {
+      if (array[row][column] === undefined) {
+        console.log(row, column, array.map(row => row.map(cell => cell.value)));
+      }
+      if (array[row][column] !== undefined && array[row][column].value === num) {
+        return false;
+      }
+    }
+
+    // toto
+    return true;
+    // Check Row contains the number
+
+    // Check block contains the number
+    const maxRows = array[0].length;
+    const currentRow = array.length + 1;
+    const rowStart = maxRows % currentRow;
+
+    for (let row = 0; row < array.length; row++) {
+      for (let col = 0; col < array[0].length; col++) {
+        if (array[row][col].value === num) {
+          return false;
+        }
+      }
+    }
     /*nums.some(cell => cell === num) || array.some((coloum: Cell[]) => coloum[col].value === num)) {
       col--;
       continue;
@@ -26,30 +57,45 @@ export class SudokuGenerator {
   }
 
   private static createRow(array: Cell[][]): Cell[] {
-    const availableNumbers = Object.assign([], this.numbers);
+    const rowAvailable = Object.assign([], this.numbers);
     const cells = [];
     const nums = [];
     let iter = 0;
     for (let col = 0; col < 9; col++) {
+      const columnNotAvailableNumbers = [];
+      for (let r = 0; r < array.length; r++) {
+        if (array[r][col] !== undefined) {
+          columnNotAvailableNumbers.push(array[r][col].value);
+        }
+      }
+
+      const currentAvailable = [];
+      rowAvailable.forEach(n => {
+        if (!columnNotAvailableNumbers.find(x => x === n)) {
+          currentAvailable.push(n);
+        }
+      });
+      // console.log('cur', currentAvailable, nums);
       iter++;
       if (iter > 15000) {
         console.warn('iter', iter);
-        break;
+        return [new Cell(-1), new Cell(-1), new Cell(-1), new Cell(-1),
+          new Cell(-1), new Cell(-1), new Cell(-1), new Cell(-1), new Cell(-1)];
       }
       // console.log(col);
-      const index = Math.floor(Math.random() * availableNumbers.length);
-      const num = availableNumbers[index];
-      if (this.canset()) {
-        availableNumbers.splice(index, 1);
+      const index = Math.floor(Math.random() * currentAvailable.length);
+      const num = currentAvailable[index];
+      if (this.canset(num, col, nums, array)) {
+        rowAvailable.splice(rowAvailable.indexOf(num, 0), 1);
         nums[col] = num;
-      }else {
-        col--;
+      } else {
+        //col--;
       }
     }
     nums.forEach(a => {
       cells.push(new Cell(a));
     });
-    console.log(cells.map(x => x.value));
+    console.log(cells.map(x => x.value), iter);
     return cells;
   }
 
