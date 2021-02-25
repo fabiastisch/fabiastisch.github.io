0245bd06262441277f5da9ece31490ff1b2c0dd8
Fabian Weber <fabiandotweber@gmail.com>
2021-02-25 20:23:18 +0100

    wip on svg images

--- src/app/chess/chess-board/chess-board-model.ts
+++ src/app/chess/chess-board/chess-board-model.ts
@@ -1,4 +1,4 @@
-import {arrayStartAtZero} from "../../util/utils-math";
+import {arrayStartAtZero} from '../../util/utils-math';
 
 export class ChessCell {
   constructor(row: number, col: number) {
@@ -8,7 +8,7 @@ export class ChessCell {
 
   row: number;
   col: number;
-  value: any;
+  value: string = '';
 }
 
 export class ChessBoardModel extends Array<Array<ChessCell>> {
@@ -22,5 +22,27 @@ export class ChessBoardModel extends Array<Array<ChessCell>> {
       });
       this.push(dataRow);
     });
+    this.reset();
+  }
+
+  public reset(): void {
+    this[0][0].value = 'blackTower';
+    this[0][1].value = 'blackKnight';
+    this[0][2].value = 'blackBishop';
+    this[0][3].value = 'blackQueen';
+    this[0][4].value = 'blackKing';
+    this[0][5].value = 'blackBishop';
+    this[0][6].value = 'blackKnight';
+    this[0][7].value = 'blackTower';
+    this[1].forEach(cell => cell.value = 'blackPawn');
+    this[6].forEach(cell => cell.value = 'whitePawn');
+    this[7][0].value = 'whiteTower';
+    this[7][1].value = 'whiteKnight';
+    this[7][2].value = 'whiteBishop';
+    this[7][3].value = 'whiteQueen';
+    this[7][4].value = 'whiteKing';
+    this[7][5].value = 'whiteBishop';
+    this[7][6].value = 'whiteKnight';
+    this[7][7].value = 'whiteTower';
   }
 }
--- src/app/chess/chess.module.ts
+++ src/app/chess/chess.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ChessComponent} from './chess.component';
 import { ChessBoardComponent } from './chess-board/chess-board.component';
+import { ChessCellComponent } from './chess-cell/chess-cell.component';
 
 
 @NgModule({
   declarations: [
     ChessComponent,
-    ChessBoardComponent
+    ChessBoardComponent,
+    ChessCellComponent
   ],
   imports: [
     CommonModule
