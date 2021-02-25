d47ddf4292ac22a0bf21a5775b3600857f5641a0
Fabian Weber <fabiandotweber@gmail.com>
2021-02-25 01:47:39 +0100

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppComponent } from './app.component';
 import {RouterModule} from '@angular/router';
 import { SudokuComponent } from './sudoku/sudoku.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SudokuGridComponent } from './sudoku/sudoku-grid/sudoku-grid.component';
 import {FormsModule} from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SudokuComponent,
-    SudokuGridComponent,
+    SudokuComponent
   ],
   imports: [
     BrowserModule,
