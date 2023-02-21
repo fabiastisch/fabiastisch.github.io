3301df9ce5af1ca768c3abd042c3816f1d081b54
Fabian Weber <fabiastisch@gmail.com>
2023-02-21 01:11:40 +0100

    add contact section

--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -3,21 +3,24 @@ import { CommonModule } from '@angular/common';
 import {SwitchComponent} from "./switch/switch.component";
 import {UnderlineComponent} from "./underline/underline.component";
 import {FormsModule} from "@angular/forms";
+import { SvgComponent } from './svg/svg.component';
 
 
 
 @NgModule({
   declarations: [
     SwitchComponent,
-    UnderlineComponent
+    UnderlineComponent,
+    SvgComponent
   ],
   imports: [
     CommonModule,
     FormsModule
   ],
-  exports: [
-    SwitchComponent,
-    UnderlineComponent
-  ]
+    exports: [
+        SwitchComponent,
+        UnderlineComponent,
+        SvgComponent
+    ]
 })
 export class UtilsModule { }
